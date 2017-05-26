dc8374ffeb11ca52687b10aac0d85313203fc8bd
Alfredo <chinochinese2@gmail.com>
2017-05-26 10:56:26 -0600

    cleaned

--- MiPrimeraLista-master/src/miprimeralista/MiPrimeraLista.java
+++ MiPrimeraLista-master/src/miprimeralista/MiPrimeraLista.java
@@ -1,21 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ 
 package miprimeralista;
 
 import collection.Lista;
-
-/**
- *
- * @author laboratorio
- */
+ 
 public class MiPrimeraLista {
-
-    /**
-     * @param args the command line arguments
-     */
+ 
     public static void main(String[] args) {
         
         Lista miLista = new Lista();
@@ -23,21 +12,20 @@ public class MiPrimeraLista {
         miLista.add(1);
         miLista.add(2);
         miLista.add(7);
-        
-        //System.out.println(miLista.getSize());
-        
-        //miLista.remove(1);
-        
-        //System.out.println(miLista.size());
-        
-        //System.out.println("*cabeza*" +miLista.getCabeza().getSig().getDato());
-        
-        //System.out.println("-*ultimo*"+miLista.getUltimo().getDato());
-        
+//        
+//        System.out.println(miLista.getSize());
+//        
+//        miLista.remove(1);
+//        
+//        System.out.println(miLista.size());
+//        
+//        System.out.println("*cabeza*" +miLista.getCabeza().getSig().getDato());
+//        
+//        System.out.println("-*ultimo*"+miLista.getUltimo().getDato());
+//        
         System.out.println("To String: " +miLista.toString());
         System.out.println("IndexOf "+miLista.indexOf(7));
         System.out.println("InIndex "+miLista.getData(2));
-        System.out.println("GetIndex "+miLista.get(2));
 //        System.out.println(miLista.size());
 //        
 //        miLista.remove(3);
